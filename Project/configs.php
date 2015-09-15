4c3afaa1a043cdb80d6d3065e7e838a855a58ca5
zafex <fajrulaz@gmail.com>
2015-09-16 04:20:17 +0700

    add configuration file to sample project

---
+++ Project/configs.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+
+	/**
+	 * index_page
+	 */
+
+	'index_page' => 'index.php',
+
+	/**
+	 * database
+	 */
+
+	'db' => array(
+		'default' => array(
+			'driver' => 'mysql',
+			'dbname' => '',
+			'host' => '',
+			'username' => '',
+			'password' => '',
+			'prefix' => 'zafex_'
+		)
+	)
+);
